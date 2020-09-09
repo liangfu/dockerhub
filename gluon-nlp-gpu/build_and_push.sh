1a98d37d20add9863117f0853d54dcab8e8584b3
Liangfu Chen <liangfc@amazon.com>
2020-09-09 17:49:06 +0000

    update docker for gpu

--- gluon-nlp-gpu/build_and_push.sh
+++ gluon-nlp-gpu/build_and_push.sh
@@ -1,7 +1,7 @@
 region=$(aws configure get region)
 account_id=$(aws sts get-caller-identity --query "Account" --output text)
 registry_uri=${account_id}.dkr.ecr.${region}.amazonaws.com.cn
-image=gluon-nlp
+image=gluon-nlp-gpu
 fullname=${registry_uri}/${image}
 
 $(aws ecr get-login --registry-ids 250779322837 --region cn-north-1 --no-include-email)
@@ -10,7 +10,7 @@ $(aws ecr get-login --registry-ids ${account_id} --region ${region} --no-include
 # If the repository doesn't exist in ECR, create it.
 aws ecr describe-repositories --repository-names "${image}" --region ${region} || aws ecr create-repository --repository-name "${image}" --region ${region}
 
-docker pull 250779322837.dkr.ecr.cn-north-1.amazonaws.com.cn/mxnet:latest
+docker pull ${registry_uri}/mxnet-gpu:latest
 docker build -t ${image} -f Dockerfile .
 
 docker run --runtime=nvidia -t ${image}
--- mxnet-gpu/build_and_push.sh
+++ mxnet-gpu/build_and_push.sh
@@ -1,12 +1,19 @@
+region=$(aws configure get region)
 account_id=$(aws sts get-caller-identity --query "Account" --output text)
 registry_uri=${account_id}.dkr.ecr.cn-north-1.amazonaws.com.cn
+image=mxnet-gpu
+fullname=${registry_uri}/${image}
 
-$(aws ecr get-login --registry-ids ${account_id} --region cn-north-1 --no-include-email)
+$(aws ecr get-login --registry-ids 250779322837 --region cn-north-1 --no-include-email)
+$(aws ecr get-login --registry-ids ${account_id} --region ${region} --no-include-email)
+
+# If the repository doesn't exist in ECR, create it.
+aws ecr describe-repositories --repository-names "${image}" --region ${region} || aws ecr create-repository --repository-name "${image}" --region ${region}
 
 docker pull ${registry_uri}/mxnet-src:latest
-docker build -t mxnet -f Dockerfile . --build-arg REGISTRY_URI=${registry_uri}
+docker build -t mxnet-gpu -f Dockerfile . --build-arg REGISTRY_URI=${registry_uri}
 
-docker run --runtime=nvidia -t mxnet-gpu
+docker run --runtime=nvidia -t ${image}
 
-docker tag mxnet-gpu ${registry_uri}/mxnet-gpu
-docker push ${registry_uri}/mxnet-gpu
+docker tag ${image} ${fullname}
+docker push ${fullname}
